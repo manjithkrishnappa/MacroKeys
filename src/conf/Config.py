dda488f1f3f19fc815d890c33a6e8ce4a43dd097
manjith <krishnappa.manjith@gmail.com>
2020-09-20 19:37:59 +0530

    misssed changes from the board class

--- src/conf/Config.py
+++ src/conf/Config.py
@@ -3,3 +3,6 @@ class Config:
     def __init__(self):
         print ("Config Constructor")
     
+    def initialize(self):
+        print ("Initilizing Config")
+        return True
--- src/macro_keys.py
+++ src/macro_keys.py
@@ -1,74 +1,37 @@
 #!/usr/bin/env python
-import subprocess
+
 import time
-import os
-from evdev import InputDevice, categorize, ecodes
 from conf.Config import Config
+from board.Board import Board
 
 class Main:
 
-  def OpenSlack(self):
-    os.system('xdotool key Super')
-    time.sleep(1)
-    os.system('xdotool type "slack"')
-    time.sleep(1)
-    os.system('xdotool key Return')
-
-  def OpenBlueJeans(self):
-    os.system('xdotool key Super')
-    time.sleep(1)
-    os.system('xdotool type "bluejeans"')
-    time.sleep(1)
-    os.system('xdotool key Return')
-
-  def SetUpWW(self):
-    cmdSetupScript = 'sh /home/mkrishnappa/Work/WFS/set_up_workspace/setup_ww_develop.sh'
-    proc1=subprocess.Popen(cmdSetupScript, shell=True, stdout=subprocess.PIPE)
-    proc1.communicate()[0]
-    #boardEvent = boardEvent.decode('UTF-8').rstrip('\n')
-
   def initialize(self):
     try:
-      cmdFindBoardEvent = 'grep -A 5 -w \'"SEM USB Keyboard"\' /proc/bus/input/devices |grep sysrq |awk \'{print $4}\''
-      proc=subprocess.Popen(cmdFindBoardEvent, shell=True, stdout=subprocess.PIPE)
-      boardEvent = proc.communicate()[0]
-      boardEvent = boardEvent.decode('UTF-8').rstrip('\n')
-      boardEventFullPath = '/dev/input/'  + boardEvent
-      print (boardEventFullPath)
-      self.dev = InputDevice(boardEventFullPath)
-      print('Starting the macro service')
-      self.dev.grab()
+      if(self.board.initialize() is not True):
+        return false
+      if(self.conf.initialize() is not True):
+        return false
       return True
     except:
-      print ('Could not get the grab board')
+      print ('Initialization Failed')
       return False
-
-  def run(self):
-    for event in self.dev.read_loop():
-      if event.type == ecodes.EV_KEY:
-        key = categorize(event)
-        if key.keystate == key.key_down:
-          print (key.keycode)
-          if key.keycode == 'KEY_KPENTER':
-            print ('Going to break out of the loop')
-            break
-          if key.keycode == 'KEY_KP0':
-            os.system('xdotool key Super;')
-            os.system('xdotool type "steam"; xdotool key Return')
-          if key.keycode == 'KEY_KP1':
-            OpenSlack()
-          if key.keycode == 'KEY_KP2':
-            OpenBlueJeans()
 
   def cleanUp(self):
       print("Clean Up called!")
-      self.dev.ungrab()
+      self.board.cleanUp()
 
   def __init__(self):
+    #Create Obejects of other classes
+    self.board = Board()
+    self.conf = Config()
+
     if(self.initialize() is not True):
       print ('Could not initialize; Exiting!')
       return
-    self.run()
+    #This will make the operation run in a loop and keep polling for input on the board
+    self.board.run()
+
     time.sleep(0.01)
     self.cleanUp()
     print("Exit!")
