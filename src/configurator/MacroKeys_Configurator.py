6584306de77458eb4186e4a34b6c4c888fd54d7b
manjith <krishnappa.manjith@gmail.com>
2020-10-01 22:17:12 +0530

    added the UI functionality to show window and clean up on close

--- src/configurator/MacroKeys_Configurator.py
+++ src/configurator/MacroKeys_Configurator.py
@@ -1,6 +1,13 @@
 import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk as gtk
+from macro_keys import Main
+
+###
+# NOTE: This file is no longer in use
+# The functionality of UI is moved to the macro_keys class
+###
+
 
 class cMain:
     def __init__(self):
@@ -12,6 +19,8 @@ class cMain:
         window.connect("delete-event", gtk.main_quit)
         window.connect("destroy", gtk.main_quit)
         window.show()
+
+        Main()
     
 if __name__ == "__main__":
     main = cMain()
--- src/macro_keys.py
+++ src/macro_keys.py
@@ -4,10 +4,14 @@ import time
 from conf.Config import Config
 from board.Board import Board
 from Profile import Profile
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk as gtk
 
 class Main:
   #TODO: This should come from a save file
   _activeProfileIndex = 0
+  _isInitialized = False
 
   def initialize(self):
     try:
@@ -17,6 +21,7 @@ class Main:
         return False
       if(self.activeProfile.initialize(self._activeProfileIndex, self.board) is False):
         return False
+      _isInitialized = True
       return True
     except:
       print ('Initialization Failed')
@@ -25,6 +30,25 @@ class Main:
   def cleanUp(self):
       print("Clean Up called!")
       self.board.cleanUp()
+  
+  def InitializeUI(self):
+    gladeFile = './ui/MacroKeys_Configurator.glade'
+    self.builder = gtk.Builder()
+    self.builder.add_from_file(gladeFile)
+
+    window = self.builder.get_object("main")
+    #window.connect("delete-event", self.cleanUpUI)
+    window.connect("destroy", self.cleanUpUI)
+    window.show()
+    print ("Should be showing the window now")
+    
+    return True
+  
+  def cleanUpUI(self, widget):
+    self.cleanUp()
+    print("Exit!")
+    gtk.main_quit(widget)
+
 
   def __init__(self):
     #Create Obejects of other classes
@@ -35,12 +59,14 @@ class Main:
     if(self.initialize() is False):
       print ('Could not initialize; Exiting!')
       return
-    #This will make the operation run in a loop and keep polling for input on the board
-    self.board.run()
 
-    time.sleep(0.01)
-    self.cleanUp()
-    print("Exit!")
+    if(self.InitializeUI() is False):
+      print ('Could not initialize UI; Exiting!')
+      return
+
+    #This will make the operation run in a loop and keep polling for input on the board
+    # self.board.run()
 
 if __name__ == "__main__":
     main = Main()
+    gtk.main()
